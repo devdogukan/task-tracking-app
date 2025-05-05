490e9980ab6f8c5af1ffeb8b303604c72548e374
devdogukan <dgnkaksy06@gmail.com>
2025-05-05 10:59:30 +0300

    feat: add Task entity with status and priority enums

---
+++ tasks/src/main/java/com/devdogukan/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.devdogukan.tasks.domain.entities;
+
+public enum TaskPriority {
+    HIGH, MEDIUM, LOW
+}
---
+++ tasks/src/main/java/com/devdogukan/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.devdogukan.tasks.domain.entities;
+
+public enum TaskStatus {
+    OPEN, CLOSED
+}
